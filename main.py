b66450701996e9d9ea8f2ebe22ed4da800fb42b0
Skydplay95 <131533231+Skydplay95@users.noreply.github.com>
2023-11-25 10:34:58 +0000

    add clear function to make console more readable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#Step 2
-
+from replit import clear
 import random
 
 stages = [
@@ -73,51 +72,53 @@ import hangman_art
 print(hangman_art.logo)
 
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
+#initialize number of lives
 lives = 6
 
+#create a display with each blank correspond to a letter of the random word
 display = []
 for letter in chosen_word:
     display.append("_")
 
 #condition to loop, purpose is to user make multiple guess
 end_game = False
+
 while end_game == False:
 
     guess = input("Guess a letter: ").lower()
 
-    #TODO-2: - Loop through each position in the chosen_word;
-    #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-    #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+    #clear the terminal so it's more readeable
+    clear()
 
     #check if guess letter by user as already been found
     if guess in display:
         print(f"You already find the {guess} letter")
 
+    #initialize count
     count = 0
+    #loop to each letter in random work
     for letter in chosen_word:
         count += 1
         if letter == guess:
+            #count - 1 to accord it to the index and replace blank by guess
             display[count - 1] = guess
-            #print("Right")
-
-    #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 
     #reduce lives by one if wrong guess and tell user if letter in the word, if live = 0 end game
     if not guess in chosen_word:
         print(f"{guess} not in the word to guess")
         lives -= 1
 
+        #if no more lives, stop the game
         print(lives)
         if lives == 0:
             end_game = True
-            print("Sorry you lose")
+            print(
+                "Sorry you lose, you haven't found the word, you'll be better next time"
+            )
 
+    #join all value of the display list
     print(f"{' '.join(display)}")
 
     #if no more blank in display game end by user win
